1b48c9e8420c0ca7d5547fb046994e46495accb2
lanMahalaxmi <141748874+lanMahalaxmi@users.noreply.github.com>
2023-08-08 22:45:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('registrationForm');
-    const tableBody = document.querySelector('#usersTable tbody');
+  const form = document.getElementById('registrationForm');
+  const tableBody = document.querySelector('#usersTable tbody');
+  
+  // Load data from localStorage on page load
+  const storedData = localStorage.getItem('userData');
+  if (storedData) {
+    const data = JSON.parse(storedData);
+    data.forEach(entry => appendRowToTable(entry));
+  }
+  
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
     
-    // Load data from localStorage on page load
-    const storedData = localStorage.getItem('userData');
-    if (storedData) {
-      const data = JSON.parse(storedData);
-      data.forEach(entry => appendRowToTable(entry));
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const acceptTerms = document.getElementById('acceptTerms').checked;
+    
+    // Additional validation for date of birth (between 18 and 55)
+    const currentDate = new Date();
+    const dobDate = new Date(dob);
+    const age = currentDate.getFullYear() - dobDate.getFullYear();
+    
+    if (age < 18 || age > 55) {
+      alert('Please enter a valid date of birth (between ages 18 and 55).');
+      return;
     }
     
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      const dob = document.getElementById('dob').value;
-      const acceptTerms = document.getElementById('acceptTerms').checked;
-      
-      // Additional validation for date of birth (between 18 and 55)
-      const currentDate = new Date();
-      const dobDate = new Date(dob);
-      const age = currentDate.getFullYear() - dobDate.getFullYear();
-      
-      if (age < 18 || age > 55) {
-        alert('Please enter a valid date of birth (between ages 18 and 55).');
-        return;
-      }
-      
-      const newRowData = {
-        name: name,
-        email: email,
-        password: password,
-        dob: dob,
-        acceptTerms: acceptTerms
-      };
-      
-      // Save data to localStorage
-      let data = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : [];
-      data.push(newRowData);
-      localStorage.setItem('userData', JSON.stringify(data));
-      
-      appendRowToTable(newRowData);
-      
-      // Reset the form
-      form.reset();
-    });
+    const newRowData = {
+      name: name,
+      email: email,
+      password: password,
+      dob: dob,
+      acceptTerms: acceptTerms
+    };
     
-    function appendRowToTable(entry) {
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-        <td>${entry.name}</td>
-        <td>${entry.email}</td>
-        <td>${entry.password}</td>
-        <td>${entry.dob}</td>
-        <td>${entry.acceptTerms ? 'Yes' : 'No'}</td>
-      `;
-      tableBody.appendChild(newRow);
-    }
+    // Save data to localStorage
+    let data = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : [];
+    data.push(newRowData);
+    localStorage.setItem('userData', JSON.stringify(data));
+    
+    appendRowToTable(newRowData);
+    
+    // Reset the form
+    form.reset();
   });
   
+  function appendRowToTable(entry) {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${entry.name}</td>
+      <td>${entry.email}</td>
+      <td>${entry.password}</td>
+      <td>${entry.dob}</td>
+      <td>${entry.acceptTerms ? 'Yes' : 'No'}</td>
+    `;
+    tableBody.appendChild(newRow);
+  }
+});
